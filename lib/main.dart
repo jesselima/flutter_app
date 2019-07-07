ccda7060be28f25f6d5e1ce92f41be0b5a5fc8b5
Jesse Lima <p000jesse.farias@prservicos.com.br>
2019-07-07 18:09:04 -0300

    RaisedButton onClick change text state

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,40 @@ void main() => runApp(
     )
 );
 
+// Attention for the circular reference for MyApp. Why?
+//...
+
 class MyApp extends StatefulWidget {
   @override
   _State createState() => new _State();
 }
 
 class _State extends State<MyApp> {
+
+  String _appBarName = "Flutter App";
+  String _value = "Hello World!";
+
+  void _onClick(){
+    setState(() {
+      _value = "Text clicked and state changed";
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    // build refer to "render". To render something into the screen.
     return new Scaffold(
       appBar: AppBar(
-        title: Text("Name Here"),
+        title: Text(_appBarName),
       ),
       body: Container(
         padding: EdgeInsets.all(32.0),
         child: Center(
           child: Column(
             children: <Widget>[
-              Text("Hello World!"),
-              Text("Another text!")
+              Text(_value),
+              RaisedButton(onPressed: _onClick, child: Text("Change Text"))
             ],
           ),
         ),
