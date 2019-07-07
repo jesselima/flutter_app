96b1b5eef332708b7f786acc092f69552d0863e7
Jesse Lima <p000jesse.farias@prservicos.com.br>
2019-07-07 20:02:39 -0300

    Switch Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MaterialApp(home: MyApp()));
-
-// Attention for the circular reference for MyApp. Why?
-//...
+void main() => runApp(
+    new MaterialApp(
+      home: MyApp()
+    )
+);
 
 class MyApp extends StatefulWidget {
   @override
@@ -14,52 +15,11 @@ class _State extends State<MyApp> {
   String _appBarName = "Flutter App";
 
   // Radios need a initial values
-  int _value1 = 0;
-  int _value2 = 0;
+  bool _value1 = false;
+  bool _value2 = false;
 
-  void _setValue1(int value) =>  setState(() => _value1 = value);
-  void _setValue2(int value) =>  setState(() => _value2 = value);
-
-  Widget makeRadios() {
-    // List list = new List(); // Leads to Error: type 'List<dynamic>' is not a subtype of type 'List<Widget>'
-    List<Widget> list = [];
-
-    for(int i = 0; i < 3; i++) {
-      list.add(Radio(
-          value: i,
-          groupValue: _value1,
-          onChanged: _setValue1
-        )
-      );
-    }
-
-    Column column = Column(children: list);
-    // Return a list inside a column
-    return column;
-  }
-
-
-  Widget makeRadiosTile() {
-    // List list = new List(); // Leads to Error: type 'List<dynamic>' is not a subtype of type 'List<Widget>'
-    List<Widget> list = [];
-
-    for(int i = 0; i < 3; i++) {
-      list.add(RadioListTile(
-          value: i,
-          groupValue: _value2,
-          onChanged: _setValue2,
-          activeColor: Colors.green,
-          controlAffinity: ListTileControlAffinity.leading,
-          title: Text("Item $i"),
-          subtitle: Text("Subtitle $i"),
-        )
-      );
-    }
-
-    Column column = Column(children: list);
-    // Return a list inside a column
-    return column;
-  }
+  void _onChangeValue1(bool value) =>  setState(() => _value1 = value);
+  void _onChangeValue2(bool value) =>  setState(() => _value2 = value);
 
 
   @override
@@ -74,13 +34,28 @@ class _State extends State<MyApp> {
         child: Center(
           child: Column(
             children: <Widget>[
-              // Generate the list of radios
-              makeRadios(),
-              makeRadiosTile()
+              Switch(
+                  value: _value1, 
+                  onChanged: _onChangeValue1,
+              ),
+              SwitchListTile(
+                value: _value2,
+                onChanged: _onChangeValue2,
+                title: Text(
+                  "Notifications",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                  ),
+                ),
+
+              )
             ],
           ),
         ),
       ),
     );
   }
+
 }
+
