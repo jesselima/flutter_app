fd42f607274c3402e3696eb53e5dd915ae2d469d
Jesse Lima <p000jesse.farias@prservicos.com.br>
2019-07-08 09:01:45 -0300

    Working with Rows, Columns and TextEditingController

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,9 @@ class _State extends State<MyApp> {
 
   String _appBarName = "Flutter App";
 
+  TextEditingController _userController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -24,10 +27,32 @@ class _State extends State<MyApp> {
       body: Container(
         padding: EdgeInsets.all(32.0),
         child: Center(
+
           child: Column(
             children: <Widget>[
-              Text("Hello World!"),
-              Text("Another text!")
+              Text("Please Login"),
+              Row(
+                children: <Widget>[
+                  Text("Username: "),
+                  //TextField(controller: _userController), // TODO -> THIS LEAD TO ERROR
+                  Expanded(child: TextField(controller: _userController),) // Expanded makes the field to fill the screen width.
+                ],
+              ),
+
+              Row(
+                children: <Widget>[
+                  Text("Password: "),
+                  Expanded(child: TextField(controller: _passwordController, obscureText: true,),) // Expanded makes the field to fill the screen.
+                ],
+              ),
+
+              Padding(
+                padding: EdgeInsets.all(12),
+                child: RaisedButton(
+                  onPressed: () => print("Login ${_userController.text}"),
+                  child: Text("Click me"),),
+              )
+              
             ],
           ),
         ),
