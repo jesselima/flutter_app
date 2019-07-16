4ed13fb575f53bdf19a0f6444b9851027c099777
jesselima <jesselima.tech@gmail.com>
2019-07-15 22:34:38 -0300

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -14,29 +14,20 @@ class MyApp extends StatefulWidget {
 
 class _State extends State<MyApp> {
 
-  String _appBarName = "Flutter App";
-  String _value = "Hello World!";
-
-  void _onClick() => setState(() => _value = DateTime.now().toString());
 
   @override
   Widget build(BuildContext context) {
 
     return new Scaffold(
       appBar: AppBar(
-        title: Text(_appBarName),
+        title: Text("Title Text"),
       ),
       body: Container(
         padding: EdgeInsets.all(32.0),
         child: Center(
           child: Column(
             children: <Widget>[
-              Text(_value),
-              IconButton(
-                icon: Icon(Icons.timer),
-                onPressed: _onClick,
-                tooltip: "Click me!",
-              )
+              Text("Some Text"),
             ],
           ),
         ),
