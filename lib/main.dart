56677da6ca3bd7506b4ed67047fd3a619f3f182a
jesselima <jesselima.tech@gmail.com>
2019-07-15 23:11:13 -0300

    Chips, Keys and Children List of Widgets. Create, Add and remove chips.

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,64 @@ class MyApp extends StatefulWidget {
 
 class _State extends State<MyApp> {
 
+  int _counter = 0;
+  List<Widget> _list = List<Widget>();
+
+
+  @override
+  void initState() {
+      for(int i = 0; i < 5; i++) {
+        Widget _child = _newItem(i);
+        _list.add(_child);
+      }
+  }
+
+
+  // TODO Info -> CLICK ACTION
+  void _onClicked() {
+    Widget child = _newItem(_counter);
+    setState(() {
+      _list.add(child);
+    });
+  }
+
+  // TODO Info -> NEW ITEM
+  Widget _newItem(int i) {
+
+    Key key = Key("item_$i"); // When we call this key, we will be referencing the Container key
+
+    Container child = Container(
+      key: key,
+      padding: EdgeInsets.all(10),
+      child: Chip(
+        label: Text("$i Name Here"),
+        deleteIconColor: Colors.red,
+        deleteButtonTooltipMessage: "Delete",
+        onDeleted: () => _removeItem(key),
+        avatar: CircleAvatar(
+          backgroundColor: Colors.grey.shade800,
+          child: Text(i.toString()),
+        ),
+      ),
+    );
+    _counter++;
+    return child;
+  }
+
+
+  // TODO Info ->  REMOVE ITEM
+  void _removeItem(Key key) {
+    for(int i = 0; i < _list.length; i++) {
+      Widget child = _list.elementAt(i);
+      if(child.key == key) {
+        setState(() {
+          _list.removeAt(i);
+          print("Removing ${key.toString()}");
+        });
+      }
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,13 +80,17 @@ class _State extends State<MyApp> {
       appBar: AppBar(
         title: Text("Title Text"),
       ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onClicked,
+        child: Icon(Icons.add),
+      ),
+
       body: Container(
         padding: EdgeInsets.all(32.0),
         child: Center(
           child: Column(
-            children: <Widget>[
-              Text("Some Text"),
-            ],
+            children: _list,
           ),
         ),
       ),
