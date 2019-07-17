c6871e17f8e2daa95d4a8fd8cb043b54ac27c090
jesselima <jesselima.tech@gmail.com>
2019-07-17 06:03:08 -0300

    Stepper Horizontal (Vertical not working)

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,51 @@ class MyApp extends StatefulWidget {
 
 class _State extends State<MyApp> {
 
+  List<Step> _steps;
+  int _current;
+
+  void _stepContinue() {
+    setState(() {
+      _current++;
+      if(_current >= _steps.length) _current = _steps.length - 1;
+    });
+  }
+
+  void _stepCancel() {
+    setState(() {
+      _current--;
+      if(_current < 0) _current = 0;
+    });
+  }
+
+  void _stepTap(int index) {
+    setState(() {
+      _current = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _current = 0;
+    _steps = <Step>[
+      Step(
+        title: Text("Step 1"),
+        content: Text("Do something"),
+        isActive: true
+      ),
+      Step(
+          title: Text("Step 1"),
+          content: Text("Do something"),
+          isActive: false
+      ),
+      Step(
+          title: Text("Step 1"),
+          content: Text("Do something"),
+          isActive: false
+      ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +70,14 @@ class _State extends State<MyApp> {
       body: Container(
         padding: EdgeInsets.all(32.0),
         child: Center(
-          child: Column(
-            children: <Widget>[
-              Text("Some Text"),
-            ],
-          ),
+          child: Stepper(
+            steps: _steps,
+            type: StepperType.vertical,  // // TODO Info ->  StepperType.vertical is not working
+            currentStep: _current,
+            onStepCancel: _stepCancel,
+            onStepContinue: _stepContinue,
+            onStepTapped: _stepTap,
+          )
         ),
       ),
     );
