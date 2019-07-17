4d6836d5d27bfdc5b474186df85358ee503bf05c
jesselima <jesselima.tech@gmail.com>
2019-07-17 07:39:12 -0300

    Charts - BarCharts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:core';
+import 'dart:math';
+
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -6,7 +10,14 @@ void main() => runApp(
     )
 );
 
-// TODO declare final global objects here
+// TODO declare final global objects or classes here
+
+class Sales {
+  String year;
+  int sales;
+
+  Sales({this.year, this.sales});
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -16,12 +27,34 @@ class MyApp extends StatefulWidget {
 class _State extends State<MyApp> {
 
   // TODO Declare state objects here
+  List<Sales> _data;
+  List<charts.Series<Sales, String>> _chartData;
 
+  void _makeData() {
+    _data = List<Sales>();
+    _chartData = List<charts.Series<Sales, String>>();
+
+    Random rdn = Random();
+    // Generates a randomData
+    for(int i = 2010; i < 2019; i++) {
+      _data.add(Sales(year: i.toString(), sales: rdn.nextInt(1000)));
+    }
+
+    _chartData.add(charts.Series(
+        id: "Sales",
+        colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
+        data: _data,
+        domainFn: (Sales sales, __) => sales.year,
+        measureFn: (Sales sales, __) => sales.sales,
+      fillPatternFn: (_, __) => charts.FillPatternType.solid,
+      displayName: "Sales"
+    ));
+  }
 
   @override
   void initState() {
     super.initState();
-    // TODO implement state here
+    _makeData();
 
   }
 
@@ -46,8 +79,10 @@ class _State extends State<MyApp> {
         child: Center(
           child: Column(
             children: <Widget>[
-              Text(_someTextValue),
-              RaisedButton(onPressed: _onClick, child: Text("Hello black Hole"))
+              Text("Sales Data"),
+              Expanded(
+                child: new charts.BarChart(_chartData),
+              )
             ],
           ),
         ),
