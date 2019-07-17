645a2d870453c1a315cead4fe47f6888ff81014a
jesselima <jesselima.tech@gmail.com>
2019-07-17 06:55:46 -0300

    reset live template (cleanup)

--- lib/main.dart
+++ lib/main.dart
@@ -6,28 +6,48 @@ void main() => runApp(
     )
 );
 
-class MyApp extends StatefulWidget {
+// TODO declare final global objects here
 
+class MyApp extends StatefulWidget {
   @override
   _State createState() => new _State();
 }
 
 class _State extends State<MyApp> {
 
+  // TODO Declare state objects here
+
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO implement state here
+
+  }
+
+  String _appBarName = "Flutter App";
+  String _someTextValue = "Hello World!";
+
+  void _onClick(){
+    setState(() {
+      _someTextValue = "Text clicked and state changed";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
 
     return new Scaffold(
       appBar: AppBar(
-        title: Text("Title Text"),
+        title: Text(_appBarName),
       ),
       body: Container(
         padding: EdgeInsets.all(32.0),
         child: Center(
           child: Column(
             children: <Widget>[
-              Text("Some Text"),
+              Text(_someTextValue),
+              RaisedButton(onPressed: _onClick, child: Text("Hello black Hole"))
             ],
           ),
         ),
