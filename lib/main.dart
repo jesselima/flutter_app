cfc328b936e8b25895b6ee2342eab59123ecfccd
jesselima <jesselima.tech@gmail.com>
2019-07-27 18:12:20 -0300

    Transformations sample - Change text form with 3 axis.

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,17 @@ class _State extends State<MyApp> {
 
   String _appBarName = "Flutter App";
 
+  double _x;
+  double _y;
+  double _z;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
+      _x = 0.0;
+      _y = 0.0;
+      _z = 0.0;
   }
 
   @override
@@ -36,7 +42,33 @@ class _State extends State<MyApp> {
           child: Column(
             children: <Widget>[
 
-              Text(_appBarName),
+              Row(
+                children: <Widget>[
+                  Text("X"),
+                  Slider(value: _x, onChanged: (double value) => setState(() => _x = value),)
+                ],
+              ),
+
+              Row(
+                children: <Widget>[
+                  Text("Y"),
+                  Slider(value: _y, onChanged: (double value) => setState(() => _y = value),)
+                ],
+              ),
+
+              Row(
+                children: <Widget>[
+                  Text("Z"),
+                  Slider(value: _z, onChanged: (double value) => setState(() => _z = value),)
+                ],
+              ),
+
+              Transform(transform: Matrix4.skewY(_y),
+                child: Transform(transform: Matrix4.skewX(_x),
+                child: Transform(transform: Matrix4.rotationZ(_z),
+                child: Padding(padding: EdgeInsets.all(10), child: Text("Hello World"))
+                ),),
+              )
 
             ],
           ),
