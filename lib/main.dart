bf001958b12b871b9fa9c1b548e36c2f5fc34678
jesselima <jesselima.tech@gmail.com>
2019-07-27 17:29:49 -0300

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -16,24 +16,11 @@ class _State extends State<MyApp> {
 
   String _appBarName = "Flutter App";
 
-  int _turns;
-  double _value;
-
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _turns = 0;
-    _value = 0.0;
-  }
 
-
-  void _onChanged(double value){
-    setState(() {
-      _turns = value.toInt();
-      _value = value;
-    });
   }
 
   @override
@@ -49,16 +36,7 @@ class _State extends State<MyApp> {
           child: Column(
             children: <Widget>[
 
-              Slider(
-                value: _value,
-                onChanged: _onChanged,
-                min: 0.0,
-                max: 4.0,
-              ),
-              RotatedBox(
-                quarterTurns: _turns,
-                child: Text("Hello World"),
-              ),
+              Text(_appBarName),
 
             ],
           ),
