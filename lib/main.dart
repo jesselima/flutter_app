8f1db6472b70c7485c965803cc2fe2952b4855aa
jesselima <jesselima.tech@gmail.com>
2019-07-27 19:23:41 -0300

    Animation Builder and Child Widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,38 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 
-class _State extends State<MyApp> {
+class _State extends State<MyApp> with TickerProviderStateMixin {
 
   String _appBarName = "Flutter App";
 
+  Animation animation;
+  AnimationController animationController;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    animationController = AnimationController(duration: Duration(milliseconds: 10000), vsync: this);
 
+    final CurvedAnimation curvedAnimation = CurvedAnimation(parent: animationController, curve: Curves.easeIn);
+    animation = Tween(begin: 0.0, end: 300.0).animate(curvedAnimation);
+
+    animationController.forward();
+
+  }
+
+  Widget builder(BuildContext context, Widget child) {
+    return Container(
+      width: animation.value,
+      height: animation.value,
+      child: FlutterLogo(),
+    );
+  }
+
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -33,13 +56,7 @@ class _State extends State<MyApp> {
       body: Container(
         padding: EdgeInsets.all(32.0),
         child: Center(
-          child: Column(
-            children: <Widget>[
-
-              Text(_appBarName),
-
-            ],
-          ),
+          child: AnimatedBuilder(animation: animation, builder: builder)
         ),
       ),
     );
