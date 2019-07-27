3b837eb2bdc8d1fcafcb6b89a5b5cf197b507a3e
jesselima <jesselima.tech@gmail.com>
2019-07-27 19:07:15 -0300

    Tween Animation - Animating a image

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -12,15 +13,34 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 
-class _State extends State<MyApp> {
+class _State extends State<MyApp> with SingleTickerProviderStateMixin {
 
   String _appBarName = "Flutter App";
 
+  Animation<double> animation;
+  AnimationController animationController;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    animationController = AnimationController(duration: Duration(milliseconds: 5000), vsync: this);
+    animation = Tween(begin: 0.0, end: 200.0).animate(animationController);
+    animation.addListener((){
+      setState(() {
+        // The state of the animation has changed. Here we track animation current state.
 
+      });
+    });
+
+    // START THE ANIMATION
+    animationController.forward();
+  }
+
+  // TODO Info ->  IF YOU DO NOT DISPOSE ANIMATION KEEP RUNNING ON BACKGROUND. AND THAT'S BAD!
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -31,17 +51,11 @@ class _State extends State<MyApp> {
         title: Text(_appBarName),
       ),
       body: Container(
+        width: animation.value,
+        height: animation.value,
         padding: EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-
-              Text(_appBarName),
-
-            ],
-          ),
-        ),
-      ),
+              child: Center(child: FlutterLogo(size: 300))
+      )
     );
   }
 
