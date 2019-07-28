1af701c222969489e639d34881735ee1dffb0b33
jesselima <jesselima.tech@gmail.com>
2019-07-28 10:23:33 -0300

    Polylines - Draw lines between points in a list a of LatLng

--- lib/main.dart
+++ lib/main.dart
@@ -18,37 +18,36 @@ class _State extends State<MyApp> {
 
   String _appBarName = "Flutter App";
 
+  List<LatLng> points;
+  MapController mapController;
+  List<Marker> markers;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    mapController = MapController();
+
+    points = List<LatLng>();
+    points.add(LatLng(41.8781, -87.6298));
+    points.add(LatLng(42.3314, -83.0458));
+    points.add(LatLng(42.7325, -84.5555));
+
+    markers = List<Marker>();
+    for (int i = 0; i < points.length; i++) {
+      markers.add(
+          Marker(
+              width: 80.0,
+              height: 80.0,
+              point: points.elementAt(i),
+              builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red))
+      );
+    }
 
   }
 
   @override
   Widget build(BuildContext context) {
-
-    var markers = <Marker>[
-      Marker(
-        width: 80.0,
-        height: 80.0,
-        point: new LatLng(41.8781, -87.6298),
-        builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red)),
-
-      Marker(
-          width: 80.0,
-          height: 80.0,
-          point: new LatLng(42.3314, -83.0458),
-          builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red)),
-
-      Marker(
-          width: 80.0,
-          height: 80.0,
-          point: new LatLng(42.7325, -84.5555),
-          builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red)),
-
-    ];
-
     return new Scaffold(
       appBar: AppBar(
         title: Text(_appBarName),
@@ -62,9 +61,10 @@ class _State extends State<MyApp> {
               // MapServices.getMapBoxAccessToken()
               Flexible(
                 child: FlutterMap(
+                  mapController: mapController,
                   options: new MapOptions(
                     center: new LatLng(41.8781, -87.6298),
-                    zoom: 13.0,
+                    zoom: 6.0,
                   ),
                   layers: [
                     TileLayerOptions(
@@ -73,6 +73,15 @@ class _State extends State<MyApp> {
                     ),
                     MarkerLayerOptions(
                       markers: markers,
+                    ),
+                    PolylineLayerOptions(
+                      polylines: [
+                        Polyline(
+                          points: points,
+                          strokeWidth: 4.0,
+                          color: Colors.purple
+                        )
+                      ]
                     ),
                   ],
                 )
