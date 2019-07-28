638f244441825649a101eccc5952e310e305d357
jesselima <jesselima.tech@gmail.com>
2019-07-27 21:30:58 -0300

    MapView with markers from OpenMaps (openstreetmap.org).
    - There is no need to use API keys.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/mapservices.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 
@@ -29,6 +28,27 @@ class _State extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
 
+    var markers = <Marker>[
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: new LatLng(41.8781, -87.6298),
+        builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red)),
+
+      Marker(
+          width: 80.0,
+          height: 80.0,
+          point: new LatLng(42.3314, -83.0458),
+          builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red)),
+
+      Marker(
+          width: 80.0,
+          height: 80.0,
+          point: new LatLng(42.7325, -84.5555),
+          builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red)),
+
+    ];
+
     return new Scaffold(
       appBar: AppBar(
         title: Text(_appBarName),
@@ -43,30 +63,16 @@ class _State extends State<MyApp> {
               Flexible(
                 child: FlutterMap(
                   options: new MapOptions(
-                    center: new LatLng(51.5, -0.09),
+                    center: new LatLng(41.8781, -87.6298),
                     zoom: 13.0,
                   ),
                   layers: [
-                    new TileLayerOptions(
-                      urlTemplate: "https://api.tiles.mapbox.com/v4/"
-                          "{id}/{z}/{x}/{y}@2x.png?access_token=${MapServices.getMapBoxAccessToken()}",
-                      additionalOptions: {
-                        'accessToken': MapServices.getMapBoxAccessToken(),
-                        'id': 'mapbox.streets',
-                      },
+                    TileLayerOptions(
+                      urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      subdomains: ['a', 'b', 'c']
                     ),
-                    new MarkerLayerOptions(
-                      markers: [
-                        new Marker(
-                          width: 80.0,
-                          height: 80.0,
-                          point: new LatLng(51.5, -0.09),
-                          builder: (ctx) =>
-                          new Container(
-                            child: new FlutterLogo(),
-                          ),
-                        ),
-                      ],
+                    MarkerLayerOptions(
+                      markers: markers,
                     ),
                   ],
                 )
