f1f976bbb9ad96119c9dd7baca56673367c69177
jesselima <jesselima.tech@gmail.com>
2019-07-28 11:46:20 -0300

    URL Launcher for: URL(web), Email, SMS and Telephone.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(
     new MaterialApp(
@@ -23,6 +24,32 @@ class _State extends State<MyApp> {
 
   }
 
+  void _showUrl() {
+    _launch("https://flutter.dev/");
+  }
+
+  void _showEmail() {
+    _launch("mailto:jesselima.tech@gmail.com");
+  }
+
+  void _showSms() {
+    _launch("sms:11981111566");
+  }
+
+  void _showTelephone() {
+    _launch("tel:11981111566");
+  }
+
+  void _launch(String urlString) async {
+    if(await canLaunch(urlString)) {
+      await launch(urlString);
+    } else {
+      throw "Could not launch url.";
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
 
@@ -34,9 +61,14 @@ class _State extends State<MyApp> {
         padding: EdgeInsets.all(32.0),
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
 
-              Text(_appBarName),
+              RaisedButton(onPressed: _showUrl,       child: Text("URL")),
+              RaisedButton(onPressed: _showEmail,     child: Text("Email")),
+              RaisedButton(onPressed: _showSms,       child: Text("SMS")),
+              RaisedButton(onPressed: _showTelephone, child: Text("Telephone")),
 
             ],
           ),
