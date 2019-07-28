bf2c425b2079ecf9dff0b95322b894012d0a29d7
jesselima <jesselima.tech@gmail.com>
2019-07-28 09:50:33 -0300

    Map with Interactions and custom markers.
    - Dynamic generated buttons
    - Button click show its associated coordinates.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 
@@ -18,64 +19,89 @@ class _State extends State<MyApp> {
 
   String _appBarName = "Flutter App";
 
+  MapController mapController;
+  Map<String, LatLng> coordinates;
+  List<Marker> markers;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    mapController = MapController();
 
+    coordinates = Map<String, LatLng>();
+    coordinates.putIfAbsent("Jhon",     () => LatLng(41.8781, -87.6298));
+    coordinates.putIfAbsent("Bill",     () => LatLng(42.3314, -83.0458));
+    coordinates.putIfAbsent("Petter",   () => LatLng(42.7325, -84.5555));
+
+    markers = List<Marker>();
+
+    for (int i = 0; i < coordinates.length; i++) {
+      markers.add(
+          Marker(
+              width: 80.0,
+              height: 80.0,
+              point: coordinates.values.elementAt(i),
+              builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red))
+      );
+    }
+  }
+
+  // Method to move the map around...
+  void _showCoordinates(int index) {
+    mapController.move(coordinates.values.elementAt(index), 8.0);
+  }
+
+  List<Widget> _makeButtons() {
+    List<Widget> list = List<Widget>();
+    for(int i = 0; i < coordinates.length; i++) {
+      list.add(
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: FlatButton(
+              onPressed: () => _showCoordinates(i),
+              child: Text(coordinates.keys.elementAt(i)),
+              color: Colors.black12,
+          ))
+
+      );
+    }
+    return list;
   }
 
   @override
   Widget build(BuildContext context) {
-
-    var markers = <Marker>[
-      Marker(
-        width: 80.0,
-        height: 80.0,
-        point: new LatLng(41.8781, -87.6298),
-        builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red)),
-
-      Marker(
-          width: 80.0,
-          height: 80.0,
-          point: new LatLng(42.3314, -83.0458),
-          builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red)),
-
-      Marker(
-          width: 80.0,
-          height: 80.0,
-          point: new LatLng(42.7325, -84.5555),
-          builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red)),
-
-    ];
-
     return new Scaffold(
       appBar: AppBar(
         title: Text(_appBarName),
       ),
       body: Container(
-        padding: EdgeInsets.all(16.0),
+        //padding: EdgeInsets.all(16.0),
         child: Center(
           child: Column(
             children: <Widget>[
 
-              // MapServices.getMapBoxAccessToken()
+              Row(
+                children: _makeButtons(),
+              ),
+
               Flexible(
-                child: FlutterMap(
-                  options: new MapOptions(
-                    center: new LatLng(41.8781, -87.6298),
-                    zoom: 13.0,
-                  ),
-                  layers: [
-                    TileLayerOptions(
-                      urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                      subdomains: ['a', 'b', 'c']
+                  child: FlutterMap(
+                    mapController: mapController,
+                    options: MapOptions(
+                      center: LatLng(41.8781, -87.6298),
+                      zoom: 13.0,
                     ),
-                    MarkerLayerOptions(
-                      markers: markers,
-                    ),
-                  ],
-                )
+                    layers: [
+                      TileLayerOptions(
+                          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                          subdomains: ['a', 'b', 'c']
+                      ),
+                      MarkerLayerOptions(
+                        markers: markers,
+                      ),
+                    ],
+                  )
               )
 
             ],
@@ -84,5 +110,4 @@ class _State extends State<MyApp> {
       ),
     );
   }
-
 }
